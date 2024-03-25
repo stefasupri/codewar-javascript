ce37c9d67d18d87f4c524c707b66fb346cb7b682
Stefa Supri <stefa.supri@gmail.com>
2024-03-25 23:37:29 +0700

    vowel

---
+++ vowel.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+
+// function replace(s){
+//     //coding and coding....
+//     return s.replace(/[aeiouAEIOU]/g,'!');
+// }
+
+const replace = (s)=> s.replace(/[aeiouAEIOU]/g,'!');
+
+console.log(replace("Hiaa!"));
